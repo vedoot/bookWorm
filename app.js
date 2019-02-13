fa35feaf37819642291be4cd059c546a2db7cc5c
Vedant <vm.WorkAddress@gmail.com>
2019-02-13 11:28:15 -0500

    headers

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ const fs = require('fs');
 var request = require('request');
 const $ = require('cheerio');
 var baseURL = 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1';
+const options = {
+    url: 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1',
+    method: 'GET',
+    headers: {
+      'Referer': 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    }
+};
+var i=0;
 
 
-function f(baseURL){
-  request(baseURL,function(err,response,html){
-    // console.log( $('p', html).text());
-    var nextURL = $('.next-part-link', html).attr("href");
-    console.log(nextURL);
+function f(options){
+  request(options,function(err,response,html){
+    var chapters = [];
+    var nextURL  = {
+        url: $('.next-part-link', html).attr("href"),
+        method: 'GET',
+        headers: {
+            'Referer': nextURL,
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        }
+    };
+    chapters.push($('p', html).text());
+    if(i=25){
+      console.log(chapters[0]);
+    }
     f(nextURL);
+    i++;
   });
 }
 
