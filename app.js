fbba5f5a9e6d0740c1bef916b8b1843eef22ce3e
Vedant <vm.WorkAddress@gmail.com>
2019-02-24 00:54:42 -0500

    housekeeping

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-const fs = require('fs');
-const rp = require('request-promise');
-const $ = require('cheerio');
+ const fs = require('fs');
+ const rp = require('request-promise');
+ const $ = require('cheerio');
 
-var x = 10;
-var options = {
+ const uA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+
+ var book = [];
+ var options = {
   url: 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1',
-  headers: {
-    'Referer': 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-  }
-};
+  headers: { 'Referer': 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1', 'User-Agent': uA}
+  };
 
-function traverseSite(options) {
-  rp(options)
+ function traverseSite(options) {
+  rp(options) // request promise
     .then(function(html) {
-      console.log($('p', html).text());
+      if (!$('.next-part-link', html).attr("href"))  // stops scraping when next link is undifined. Returns chapters as param for .then()
+        return chapters
 
-      var nextURL = {
-        url: $('.next-part-link', html).attr("href"),
-        method: 'GET',
-        headers: {
-          'Referer': nextURL,
-          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-        }
-      };
-      return nextURL;
+      var chapters = $('p', html).text();  // p tags on site store all book text.
+      book.push(chapters);  //moving pages into chapters array
+
+      console.log(`Chapter ${book.length} added to book!`);
+
+      return traverseSite({ url: $('.next-part-link', html).attr("href"),
+      headers: { 'Referer': $('.next-part-link', html).attr("href"),'User-Agent': uA }
+      });
     })
-    .then(function(nextURL) {
-      if (!nextURL)
-        return;
-      traverseSite(nextURL);
+    .then(function cleanText(book) {
+      if(Array.isArray(book))
+        console.log(book.toString());
+
+    })
+    .then(function formatText(cleanBook){
+
+    })
+    .then(function publish(formatedText){
+
     })
     .catch(function(err) {
       console.log(err);
@@ -37,4 +42,4 @@ function traverseSite(options) {
 }
 
 
-traverseSite(options.url);
+   traverseSite(options.url);
