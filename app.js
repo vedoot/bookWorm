fe83c055a94ccaf31bb691fd10518a5a1416e556
Vedant <vm.WorkAddress@gmail.com>
2019-02-26 20:23:32 -0500

    sexxyyy

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
  const fs = require('fs');
  const rp = require('request-promise');
- const $ = require('cheerio');
+ const cheerio = require('cheerio');
 
  const uA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
 
  var book = [];
  var options = {
-  url: 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1',
-  headers: { 'Referer': 'https://www.wattpad.com/30050343-forever-judy-blume-chapter-1', 'User-Agent': uA}
+  url: 'https://www.wattpad.com/591602002-carpe-noctem-intro',
+  headers: { 'Referer': 'https://www.wattpad.com/591602002-carpe-noctem-intro', 'User-Agent': uA}
   };
 
  function traverseSite(options) {
   rp(options) // request promise
     .then(function(html) {
-      //WHY NO WORK?!?!?!
-      $('.text',html).remove();
-      ///WTF
+      $ = cheerio.load(html);
+      $('.text').remove();
       if (!$('.next-part-link', html).attr("href"))  // stops scraping when next link is undifined. Returns chapters as param for .then()
         return book
-      var chapters = $('p',html).text();  // p tags on site store all book text.
+
+      var chapters = $('p').text();
+         // p tags on site store all book text.
       book.push(chapters);  //moving pages into chapters array
 
       console.log(`Downloaded chapter ${book.length}`)
@@ -27,12 +28,14 @@
       headers: { 'Referer': $('.next-part-link', html).attr("href"),'User-Agent': uA }
       });
     })
+
+
     .then(function cleanText(book) {
       if(Array.isArray(book))
-        console.log(`Enjoy \n\n\n ${book.toString()}`);
-    })
-    .then(function formatText(cleanBook){
-
+        book.map((x,i) => {
+          console.log(`CHAPTER ${i} \n\n${book[i]} \n\n\n`);
+        })
+        // console.log(`Enjoy \n\n\n ${book.toString()}\n\n\n`);
     })
     .then(function publish(formatedText){
 
