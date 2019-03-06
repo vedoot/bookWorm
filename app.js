dd406bb00854dd20f6462420326e4fd9ea3eaa5c
Vedant <vm.WorkAddress@gmail.com>
2019-03-06 03:52:05 -0500

    Docx is too long to turn into PDF - figure out new solution

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
  const fs = require('fs');
  const rp = require('request-promise');
+ const errors = require('request-promise/errors');
  const cheerio = require('cheerio');
  const docx = require('docx');
+ const docxConverter = require('docx-pdf');
  const {
    Document,
    Paragraph,
@@ -19,7 +21,7 @@
  //     after: 120
  //   });
 
- const uA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36';
+ const uA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36';
 
  var book = [];
  var options = {
@@ -33,13 +35,20 @@
  var i = 0;
  var pages = [];
  var empty = new docx.Paragraph(`\n`);
+
  function traverseSite(options) {
    rp(options) // request promise
      .then(function(html) {
        $ = cheerio.load(html);
 
-       var chapterTitle = $('.item-title').text();
-       var heading = new docx.Paragraph("Amazing Heading").heading2().center();
+
+
+       doc.addParagraph(new docx.Paragraph($('.item-title').text()).thematicBreak());
+       doc.addParagraph(empty);
+       doc.addParagraph(new docx.Paragraph($('.author.h6').text()).pageBreak());
+
+
+       var heading = new docx.Paragraph($('h2').text()).heading2().center();
        doc.addParagraph(heading);
        doc.addParagraph(empty);
        $('.text').remove();
@@ -50,27 +59,33 @@
        });
        var titleOfBook = $('h3').text();
 
-       console.log(book.length);
-
        var packer = new docx.Packer();
        packer.toBuffer(doc).then((buffer) => {
          fs.writeFileSync(`freeB00k.docx`, buffer);
        });
-
+       console.log($('.next-part-link', html).attr("href") + i);
        // console.log(`Downloaded chapter ${chapterTitle}`.padStart(130));
-       if (!$('.next-part-link', html).attr("href") || i==5) // stops scraping when next link is undifined. Returns chapters as param for .then()
-         return doc;
-       i++;
-
+       if (!$('.next-part-link', html).attr("href") || i==25){
+          docxConverter('freeB00k.docx','output.pdf',function(err,result){
+                  if(err){
+                    console.log("youre fucked");
+                  }
+                  console.log('result'+result);
+                });
+            return;
+       } // stops scraping when next link is undifined. Returns chapters as param for .then()
+       else{
+         i++;
        return traverseSite({
          url: $('.next-part-link', html).attr("href"),
          headers: {
            'Referer': $('.next-part-link', html).attr("href"),
            'User-Agent': uA
-         }
+         },
        });
-     }).catch(console.log);
+     }
+     })
+     .catch(console.log);
  }
-
 
  traverseSite(options.url);
